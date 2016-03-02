e2bc3e3cc5dcfd499332e4f4dd10ebccc6877849
mark1357945 <maytel1357945@hotmail.com>
2016-03-02 11:49:09 +0800

    C# GUI
    C# GUI模板完成
    Kinect和matlab還沒加入

---
+++ SymmetryMeasurment/SymmetryMeasurment/SymmetryMeasurment/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace SymmetryMeasurment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox_Past = new System.Windows.Forms.GroupBox();
+            this.groupBox_Self = new System.Windows.Forms.GroupBox();
+            this.pictureBox_main = new System.Windows.Forms.PictureBox();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_pause = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_directory = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_subjectID = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_main)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox_Past
+            // 
+            this.groupBox_Past.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox_Past.Font = new System.Drawing.Font("標楷體", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox_Past.Location = new System.Drawing.Point(1042, 31);
+            this.groupBox_Past.Name = "groupBox_Past";
+            this.groupBox_Past.Size = new System.Drawing.Size(318, 777);
+            this.groupBox_Past.TabIndex = 0;
+            this.groupBox_Past.TabStop = false;
+            this.groupBox_Past.Text = "與過去比較";
+            // 
+            // groupBox_Self
+            // 
+            this.groupBox_Self.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox_Self.Font = new System.Drawing.Font("標楷體", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox_Self.Location = new System.Drawing.Point(706, 31);
+            this.groupBox_Self.Name = "groupBox_Self";
+            this.groupBox_Self.Size = new System.Drawing.Size(318, 777);
+            this.groupBox_Self.TabIndex = 1;
+            this.groupBox_Self.TabStop = false;
+            this.groupBox_Self.Text = "自身對稱性";
+            // 
+            // pictureBox_main
+            // 
+            this.pictureBox_main.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox_main.Location = new System.Drawing.Point(52, 191);
+            this.pictureBox_main.Name = "pictureBox_main";
+            this.pictureBox_main.Size = new System.Drawing.Size(632, 550);
+            this.pictureBox_main.TabIndex = 2;
+            this.pictureBox_main.TabStop = false;
+            // 
+            // button_start
+            // 
+            this.button_start.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_start.Location = new System.Drawing.Point(167, 771);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(101, 37);
+            this.button_start.TabIndex = 4;
+            this.button_start.Text = "開始";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.Start_Capture);
+            // 
+            // button_pause
+            // 
+            this.button_pause.Enabled = false;
+            this.button_pause.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button_pause.Location = new System.Drawing.Point(358, 771);
+            this.button_pause.Name = "button_pause";
+            this.button_pause.Size = new System.Drawing.Size(101, 37);
+            this.button_pause.TabIndex = 5;
+            this.button_pause.Text = "暫停";
+            this.button_pause.UseVisualStyleBackColor = true;
+            this.button_pause.Click += new System.EventHandler(this.Pause_Capture);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(571, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 37);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "選擇";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Choose_MainDirectory);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(37, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 19);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "資料存放位置：";
+            // 
+            // textBox_directory
+            // 
+            this.textBox_directory.Enabled = false;
+            this.textBox_directory.Font = new System.Drawing.Font("Buxton Sketch", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_directory.Location = new System.Drawing.Point(49, 130);
+            this.textBox_directory.Name = "textBox_directory";
+            this.textBox_directory.Size = new System.Drawing.Size(477, 37);
+            this.textBox_directory.TabIndex = 9;
+            this.textBox_directory.Text = "C:\\";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Buxton Sketch", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1166, 834);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Developed by Alfred Mai ...2016.03";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("標楷體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(99, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 19);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "病患ID：";
+            // 
+            // textBox_subjectID
+            // 
+            this.textBox_subjectID.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_subjectID.Location = new System.Drawing.Point(195, 46);
+            this.textBox_subjectID.Name = "textBox_subjectID";
+            this.textBox_subjectID.Size = new System.Drawing.Size(477, 36);
+            this.textBox_subjectID.TabIndex = 12;
+            this.textBox_subjectID.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("標楷體", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(5, 828);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 24);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "使用說明";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Show_Instrument);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1410, 855);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox_subjectID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_directory);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button_pause);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.pictureBox_main);
+            this.Controls.Add(this.groupBox_Self);
+            this.Controls.Add(this.groupBox_Past);
+            this.Name = "Form1";
+            this.Text = "Symmetry Measurement";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_main)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox_Past;
+        private System.Windows.Forms.GroupBox groupBox_Self;
+        private System.Windows.Forms.PictureBox pictureBox_main;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_pause;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_directory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_subjectID;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ SymmetryMeasurment/SymmetryMeasurment/SymmetryMeasurment/Form1.cs
@@ -0,0 +1,218 @@
+﻿/*
+ * 
+ * 對稱性分析軟體Symmetry Measurement的GUI介面, 需先開啟Matlab分析軟體Symmetry Calculator搭配使用
+ * 
+ * Instrument
+ *  1.輸入"病患ID"、選擇"資料存放資料夾"路徑
+ *  2.按下"開始", 病患開始動作
+ *  3.錄製結束, 按下"暫停"
+ * 
+ * Process
+ *  1.按下"開始"錄製骨架資料, 結束後資料會丟到選定資料夾內
+ *  2.Matlab分析軟體Symmetry Calculator在選定資料夾捕捉到新的骨架資料後, 計算對稱性結果並將他輸出到選定資料夾
+ *  3.Symmetry Measurement捕捉到結果後, 顯示在右側的結果欄上
+ * 
+ * Input
+ *  經由Kinect錄影獲得的人體骨架資料
+ * 
+ * Output
+ *  顯示此人的對稱性量化結果於程式介面
+ *  
+ * Extra
+ *  1.Symmetry Measurement和Symmetry Calculator所選用的資料夾必須為同一個
+ *  2.若想將此次結果存入使用者以前的紀錄, 需要將產出的資料手動複製到該病患名稱的資料夾下 
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace SymmetryMeasurment
+{
+    public partial class Form1 : Form
+    {
+        ///////////////////////////////////////////////// 
+        /////////////       參數設定       //////////////
+        ///////////////////////////////////////////////// 
+
+            
+
+        ////////////////////////////////////////////////
+        /////////////       公用變數       /////////////
+        //////////////////////////////////////////////// 
+        
+        // 暫停的時候有骨架也不會抓資料
+        Boolean isStart = false;
+
+        // 按下開始時, 將選擇的資料夾路徑存起來
+        String DirectoryPath = "";
+        String SubjectID = ""; 
+        
+        //////////////////////////////////////////////// 
+        //////////////       函式區       //////////////
+        //////////////////////////////////////////////// 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // 選擇Data所在的資料夾
+        private void Choose_MainDirectory(object sender, EventArgs e)
+        {
+            FolderBrowserDialog path = new FolderBrowserDialog();
+            path.ShowDialog();
+            this.textBox_directory.Text = path.SelectedPath;
+        }
+
+        // 顯示使用說明
+        private void Show_Instrument(object sender, EventArgs e)
+        {
+            MessageBox.Show(" 對稱性分析軟體Symmetry Measurement的GUI介面, \n"
+                            + " 需先開啟Matlab分析軟體Symmetry Calculator搭配使用\n"
+                            + "\n"
+                            + " Instrument\n"
+                            + "  0.執行Matlab分析軟體Symmetry Calculator, 讓其開著執行\n"
+                            + "  1.輸入\"病患ID\"、選擇\"資料存放資料夾\"路徑\n"
+                            + "  2.按下\"開始\", 病患開始動作\n"
+                            + "  3.錄製結束, 按下\"暫停\", 觀看結果\n"
+                            + " \n"
+                            + " Process\n"
+                            + "  1.按下\"開始\"錄製骨架資料, 結束後資料會丟到選定資料夾內\n"
+                            + "  2.Matlab分析軟體Symmetry Calculator在選定資料夾捕捉到新\n"
+                            + "    的骨架資料後, 計算對稱性結果並將他輸出到選定資料夾\n"
+                            + "  3.Symmetry Measurement捕捉到結果後, 顯示在右側的結果欄上\n"
+                            + " \n"
+                            + " Input\n"
+                            + "  經由Kinect錄影獲得的人體骨架資料\n"
+                            + " \n"
+                            + " Output\n"
+                            + "  顯示病患的對稱性量化結果於程式介面\n"
+                            + "  \n"
+                            + " Extra\n"
+                            + "  1.Symmetry Measurement和Symmetry Calculator所選用的資料夾\n"
+                            + "    必須為同一個\n"
+                            + "  2.若想將此次結果存入使用者以前的紀錄, \n"
+                            + "    需要將產出的資料手動複製到該病患名稱的資料夾下 \n"
+                            + " \n"
+                            + "\n");
+        }
+
+        // 改變isStart來改變"抓資料/暫停抓資料"狀態
+        // 按下開始檢查資料夾是否存在, 沒有的話創造
+        private void Start_Capture(object sender, EventArgs e)
+        {
+            button_start.Enabled = false;
+            button_pause.Enabled = true;
+            DirectoryPath = textBox_directory.Text;
+            SubjectID = textBox_subjectID.Text;
+
+            if (!Directory.Exists(DirectoryPath+"//Old Data"))
+            {
+                //新增Past資料夾
+                Directory.CreateDirectory(DirectoryPath+"//Old Data");
+            }
+            
+            if (!Directory.Exists(DirectoryPath + "//Old Data//" + SubjectID))
+            {
+                //新增使用者資料夾
+                Directory.CreateDirectory(DirectoryPath + "//Old Data//" + SubjectID);
+            }
+
+            isStart = true;
+        }
+        private void Pause_Capture(object sender, EventArgs e)
+        {
+            button_pause.Enabled = false;
+            button_start.Enabled = true;
+            isStart = false;
+        }
+
+
+        // 將對稱性的計算結果, 以文字顯示在GroupBox_Self和GroupBox_Past內
+        private void Show_result(object sender, EventArgs e) //******************
+        {
+            ColorLabel label = new ColorLabel();
+            label.Text = "ABC";
+            label.AppendText("DEF", Color.Red);
+            label.AppendText("G", Color.Blue);
+            label.AppendText("H", Color.Yellow);
+            label.Location = new Point(23, 30);
+            label.Font = new Font("標楷體", 16, FontStyle.Italic);
+            label.Size = new Size(250, 500);
+            groupBox_Self.Controls.Add(label);
+        }
+        
+        ////////////============================= Reference ===================================//////////////
+        ////////////============================= Reference ===================================//////////////
+
+        // 一個ColorLabel可以使用很多個顏色文字
+        class ColorLabel : Control
+        {
+            public ColorLabel()
+                : base()
+            {
+                this.textList = new List<string>();
+                this.colorList = new List<Color>();
+                this.Padding = new Padding(0);
+            }
+
+            List<string> textList;
+            List<Color> colorList;
+
+            public void AppendText(string text, Color color)
+            {
+                if (!string.IsNullOrEmpty(text))
+                {
+                    this.textList.Add(text);
+                    this.colorList.Add(color);
+                    base.Text = string.Concat(this.textList.ToArray());
+                }
+            }
+
+            public override string Text
+            {
+                get
+                {
+                    return base.Text;
+                }
+                set
+                {
+                    //base.Text = value;                                 
+                    this.textList.Clear();
+                    this.colorList.Clear();
+                    this.AppendText(value, Color.Empty);
+                }
+            }
+
+            protected override void OnPaint(PaintEventArgs e)
+            {
+                base.OnPaint(e);
+                float x = 0;
+                for (int i = 0; i < this.textList.Count; i++)
+                {
+                    Color c = this.colorList[i];
+                    using (Brush brush = new SolidBrush(c == Color.Empty ? this.ForeColor : c))
+                    {
+                        string str = this.textList[i];
+                        e.Graphics.DrawString(str, this.Font, brush, x, 0);
+                        x += e.Graphics.MeasureString(str, this.Font, this.ClientSize.Width, StringFormat.GenericTypographic).Width;
+                        if (x > this.ClientSize.Width)
+                            break;
+                    }
+                }
+            }
+        }
+
+        ////////////============================= Reference ===================================//////////////
+        ////////////============================= Reference ===================================//////////////
+    }
+}
